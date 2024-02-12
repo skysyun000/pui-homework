04095c20745e6398c028d0b7da438eff14bca3b8
skysyun000 <143558832+skysyun000@users.noreply.github.com>
2024-02-12 14:25:39 -0500

    hw-3 first draft

---
+++ solution-hw3/js/app.js
@@ -0,0 +1,53 @@
+const basePrice = 2.49;
+let glazePrice = 0.0;
+let packPrice = 1;
+
+const glazingPrice = {
+    "Keep original": 0,
+    "Sugar milk": 0,
+    "Vanilla milk": 0.5,
+    "Double chocolate": 1.5
+}
+
+let packingPrice = {
+    "1": 1,
+    "3": 3,
+    "6": 6,
+    "12": 12
+}
+
+for (let key in glazingPrice){
+    let option1 = document.createElement('option');
+    option1.text = key;
+    option1.value = glazingPrice[key];
+    let select = document.getElementById("glazingOptions");
+    select.appendChild(option1);
+}
+
+for (let key in packingPrice){
+    let option2 = document.createElement('option');
+    option2.text = key;
+    option2.value = packingPrice[key];
+    let select = document.getElementById("packingOptions");
+    select.appendChild(option2);
+}
+
+function glazingChange() {
+    let selectGlaze = document.querySelector('#glazingOptions');
+    const priceChangeG = Number(selectGlaze.value);
+    glazePrice = priceChangeG;
+    updatePrice();
+} 
+
+function packingChange() {
+    let selectPack = document.querySelector('#packingOptions');
+    const priceChangeP = Number(selectPack.value);
+    packPrice = priceChangeP;
+    updatePrice();
+} 
+
+function updatePrice(){
+    let updatedPrice = (basePrice + glazePrice) * packPrice;
+    let costElement = document.querySelector('.cost');
+    costElement.innerText = "$"+ updatedPrice.toFixed(2);
+}
